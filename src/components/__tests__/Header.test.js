c9bcdf08c2eb35b84a77dfd6b03f491fff4f33cb
kushalnigam0604 <nigamkushal0@gmail.com>
2023-07-01 23:54:14 +0530

    ep-13-live

---
+++ src/components/__tests__/Header.test.js
@@ -0,0 +1,46 @@
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import Header from "../Header";
+import store from "../../utils/store";
+import { StaticRouter } from "react-router-dom/server";
+
+test("Logo should load on rendering header", () => {
+  const header = render(
+    <StaticRouter>
+      <Provider store={store}>
+        <Header />
+      </Provider>
+    </StaticRouter>
+  );
+  const logo = header.getAllByTestId("logo");
+
+  expect(logo[0].src).toBe("http://localhost/dummy.png");
+});
+
+
+test("Online status should be green on rendering header", () => {
+  const header = render(
+    <StaticRouter>
+      <Provider store={store}>
+        <Header />
+      </Provider>
+    </StaticRouter>
+  );
+  const onlineStatus = header.getByTestId("online-status");
+
+  expect(onlineStatus.innerHTML).toBe("✅");
+});
+
+
+test("Cart should have 0 items on rendering header", () => {
+  const header = render(
+    <StaticRouter>
+      <Provider store={store}>
+        <Header />
+      </Provider>
+    </StaticRouter>
+  );
+  const cart = header.getByTestId("cart");
+
+  expect(cart.innerHTML).toBe("Cart-0 ");
+});
